5209a1a2990d1cc48301a18b3b0624981e01dffb
LeonardoFreitas <leonardofr.rocha@hotmail.com>
2023-03-31 22:43:46 -0300

    update/agora recebendo nome e lang via parametros

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const app = express();
 // Express vai usar o EJS como view engine
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
+app.get("/:nome/:lang", (req, res) => {
 
-    let nome = "Leonardo";
-    let lang = "Javascript";
+    let nome = req.params.nome;
+    let lang = req.params.lang;
 
     // Desta forma, o render irá buscar o arquivo html (.ejs) index SEMPRE dentro da pasta views (os htmls da app TEM que estar lá)
     res.render("index", { // Dados que podemos capturar no html
