6adef5c6a2c597cbe8e6f0c1ed6d4d0c36b4fe94
LeonardoFreitas <leonardofr.rocha@hotmail.com>
2023-04-05 02:12:26 -0300

    update/instalando bootstrap no projeto

--- index.js
+++ index.js
@@ -4,18 +4,36 @@ const app = express();
 // Express vai usar o EJS como view engine
 app.set("view engine", "ejs");
 
-app.get("/:nome/:lang", (req, res) => {
+// Para a aplicação aceitar arquivos estaticos que deverão estar em uma pasta com o nome "public" (arquivos de img, css ...)
+app.use(express.static("public"));
 
-    let nome = req.params.nome;
-    let lang = req.params.lang;
+app.get("/", (req, res) => {
+
+    /*
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca-Cola", preco: 5.5},
+        {nome: "Leite", preco: 4.29}
+    ];
+    */
 
     // Desta forma, o render irá buscar o arquivo html (.ejs) index SEMPRE dentro da pasta views (os htmls da app TEM que estar lá)
+    /*
     res.render("index", { // Dados que podemos capturar no html
         nome: nome,
         lang: lang,
         empresa: "VASP",
-        inscritos: 8040
+        inscritos: 8040,
+        msg: exibirMsg,
+        produtos: produtos
     });
+    */
+
+    res.render("index");
 });
 
 app.listen(8080, () => {
